4b52201ae83fe545a8d2455971679d6f70cc7bba
sophiaburkart <sophia-alena.burkart@informatik.hs-fulda.de>
2023-02-22 13:08:09 +0100

    link figma Theresa.

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:projects/screens/pre_loading_screen.dart';
 
 // Design in
 // https://www.figma.com/file/KG73Ctua7GSmwbwu3uWXvG/Prototyp-1?node-id=0%3A1&t=qwvKkf7XLdVeYxtk-0
+//https://www.figma.com/file/0FjcfC8V4czugJ7WvSbPNw/RIDA-APP
 
 class MyCustomScrollBehavior extends MaterialScrollBehavior {
   // Override behavior methods and getters like dragDevices
