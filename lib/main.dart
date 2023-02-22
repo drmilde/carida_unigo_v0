d5ea208807cc7fc3fc864bdbe5d83be675ab0dc3
drmilde <milde@hs-fulda.de>
2023-02-22 19:11:51 +0100

    Link in main.dart eingefügt

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,12 @@ import 'package:projects/screens/pre_loading_screen.dart';
 // https://www.figma.com/file/KG73Ctua7GSmwbwu3uWXvG/Prototyp-1?node-id=0%3A1&t=qwvKkf7XLdVeYxtk-0
 // https://www.figma.com/file/0FjcfC8V4czugJ7WvSbPNw/RIDA-APP
 
+
+// Hier das Dokument zur Integrationsplanung
+//
+// https://hbx.fhhrz.net/getlink/fiMADdiYYFoyhRPchhwaZN/Integrationsplanung.docx
+
+
 class MyCustomScrollBehavior extends MaterialScrollBehavior {
   // Override behavior methods and getters like dragDevices
   @override
