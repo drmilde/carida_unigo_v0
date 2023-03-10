f5d34d61fce688fbfdc48909b8b3030a272d3a9d
sophiaburkart <sophia-alena.burkart@informatik.hs-fulda.de>
2023-03-10 12:21:30 +0100

    DatenschutzScreen angepasst.

--- lib/sb/screens/einstellungen/datenschutz_screen.dart
+++ lib/sb/screens/einstellungen/datenschutz_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 import 'package:projects/sb/screens/einstellungen/einstellungen_screen.dart';
+
+import '../../../services/controller/ug_state_controller.dart';
 
 class DatenschutzScreen extends StatefulWidget {
   const DatenschutzScreen({Key? key}) : super(key: key);
@@ -8,8 +13,28 @@ class DatenschutzScreen extends StatefulWidget {
   State<DatenschutzScreen> createState() => _DatenschutzScreenState();
 }
 
+class Box {
+  String label1;
+  String label2;
+  bool status;
+
+  Box(this.label1, this.label2, this.status);
+}
+
 class _DatenschutzScreenState extends State<DatenschutzScreen> {
+  UGStateController _controller = Get.find();
+  List<Box> boxen = [];
+
   @override
+
+  void initState() {
+    boxen = [
+      Box("Standort", 'Datenschutzerklärung', false),
+    ];
+    // TODO: implement initState
+    super.initState();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar('Datenschutz', context),
@@ -17,25 +42,22 @@ class _DatenschutzScreenState extends State<DatenschutzScreen> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: 48,
-                width: double.infinity,
-                color: Colors.green,
-                child: Center(
-                  child: Text("Standort  AN/AUS"),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                height: 48,
-                width: double.infinity,
-                color: Colors.amber,
-                child: Center(
-                  child: Text("Datenschutzerklärung"),
+            Container(
+              //height: 100,
+              child: Expanded(
+                child: ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  itemCount: boxen.length,
+                  itemBuilder: (context, index) {
+                    return Column(
+                        children: [
+                        _datenschutz(boxen[index]),
+                    SizedBox(
+                    height: 8,
+                    ),
+                    ],
+                    );
+                  },
                 ),
               ),
             ),
@@ -63,6 +85,131 @@ class _DatenschutzScreenState extends State<DatenschutzScreen> {
         },
       ),
       actions: [],
+    );
+  }
+
+  Widget _datenschutz(Box box) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Container(
+        width: double.infinity,
+        margin: EdgeInsets.fromLTRB(0, 10, 0, 20),
+        padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+        decoration: BoxDecoration(
+          color: _controller.appConstants.white,
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 4,
+              blurRadius: 7,
+              offset: Offset(0, 3), // changes position of shadow
+            ),
+          ],
+        ),
+        //color: Colors.yellow,
+        child: Column(
+          children: [
+            Container(
+              child: Row(
+                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      child: Text(
+                        box.label1,
+                        style: TextStyle(
+                          fontSize: 15,
+                          //fontWeight: FontWeight.bold,
+                        ),
+                        //textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      child: FlutterSwitch(
+                        width: 100.0,
+                        height: 35.0,
+                        valueFontSize: 17.0,
+                        activeTextFontWeight: FontWeight.normal,
+                        inactiveTextFontWeight: FontWeight.normal,
+                        toggleSize: 23.0,
+                        value: box.status,
+                        borderRadius: 20.0,
+                        padding: 8.0,
+                        showOnOff: true,
+                        onToggle: (val) {
+                          setState(() {
+                            box.status = val;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            _buildShadowLine(),
+            SizedBox(
+              height: 16,
+            ),
+            Container(
+              child: Row(
+                //mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Container(
+                      child: Text(
+                        box.label2,
+                        style: TextStyle(
+                          fontSize: 15,
+                          //fontWeight: FontWeight.bold,
+                        ),
+                        //textAlign: TextAlign.left,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      child: Icon(
+                        Icons.keyboard_arrow_right,
+                        size: 50,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _buildShadowLine() {
+    return Container(
+      height: 2,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: _controller.appConstants.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.2),
+            spreadRadius: 4,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
     );
   }
 }
